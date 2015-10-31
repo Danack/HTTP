2c95b9ff169a52dc8bb121960fa5cf975adc4599
Danack <Danack@basereality.com>
2015-10-31 16:56:04 +0000

    Added header set for holding header values.

---
+++ src/Room11/HTTP/HeadersSet.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Room11\HTTP;
+
+/**
+ * Class HeaderSet
+ * Stores a set of headers. Useful for building up the set of
+ * headers needed to create a response object, instead of having
+ * to use the full response object everywhere.
+ * 
+ * Each header can have multiple values stored for it, to support
+ * use-cases which require multiple headers with the same name.
+ */
+class HeadersSet
+{
+    private $headers = [];
+    private $ucNames = [];
+
+    /**
+     * Does the response currently have a value for the specified header field?
+     *
+     * Header field names are NOT case-sensitive.
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function hasHeaders($name)
+    {
+        $ucName = strtoupper($name);
+
+        return isset($this->ucNames[$ucName]);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function addHeader($name, $value)
+    {
+        $ucField = strtoupper($name);
+        
+        if (isset($this->ucNames[$ucField])) {
+            // There's already a case-insensitive match for this field name. Reuse the
+            // existing case mapping.
+            $name = $this->ucNames[$ucField];
+        }
+
+        $this->headers[$name][] = $value;
+        $this->ucNames[$ucField] = $name;
+    }
+
+    /**
+     * Retrieve assigned header values for the specified field name
+     *
+     * @param string $name
+     * @throws \Room11\HTTP\HTTPException
+     * @return string|array
+     */
+    public function getHeaders($name)
+    {
+        $ucName = strtoupper($name);
+        
+        if (isset($this->ucNames[$ucName])) {
+            $name = $this->ucNames[$ucName];
+            return $this->headers[$name];
+        }
+
+        throw new HTTPException(
+            sprintf('Header field is not assigned: %s', $name)
+        );
+    }
+
+    /**
+     * Remove all entries for a particular header name
+     * @param $name 
+     */
+    public function clearHeaders($name)
+    {
+        $ucName = strtoupper($name);
+        if (isset($this->ucNames[$ucName])) {
+            $name = $this->ucNames[$ucName];
+            unset($this->ucNames[$ucName]);
+            unset($this->headers[$name]);
+        }
+    }
+
+    /**
+     * Reset this class to be empty.
+     */
+    public function clearAllHeaders()
+    {
+        $this->ucNames = [];
+        $this->headers = [];
+    }
+    
+    
+    /**
+     * @return array
+     */
+    public function getAllHeaders()
+    {
+        return $this->headers;
+    }
+}
